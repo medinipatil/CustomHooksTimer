b7561adca330d79bec4a614a422ef5fdd41c47ad
medinipatil <85755524+medinipatil@users.noreply.github.com>
2023-07-22 00:35:48 +0530

    Initial commit

---
+++ src/components/BackwardCounter.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+// import Card from './Card';
+
+// const BackwardCounter = () => {
+//   const [counter, setCounter] = useState(0);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCounter((prevCounter) => prevCounter - 1);
+//     }, 1000);
+
+//     return () => clearInterval(interval);
+//   }, []);
+
+//   return <Card>{counter}</Card>;
+// };
+
+// export default BackwardCounter;
+import Card from "./Card";
+import useCounter from "../hooks/use-counter";
+const BackwardCounter = () => {
+  const counter = useCounter(false);
+
+  return <Card>{counter}</Card>;
+};
+export default BackwardCounter;
